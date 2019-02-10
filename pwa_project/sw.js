697dfabe415d388cd113d89bd45bab48e828c0ab
Husnain Tariq <34330363+hxnain619@users.noreply.github.com>
2019-02-11 02:01:31 +0500

    Update sw.js

--- pwa_project/sw.js
+++ pwa_project/sw.js
@@ -1,10 +1,9 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-    './',
+    '/',
     './index.html',
     './index.js'
 ];
-
 self.addEventListener('install', function (event) {
     // Perform install steps
     event.waitUntil(
@@ -15,53 +14,38 @@ self.addEventListener('install', function (event) {
             })
     );
 });
+
+
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
             .then(function (response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-
-                return fetch(event.request).then(
-                    function (response) {
-                        // Check if we received a valid response
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // IMPORTANT: Clone the response. A response is a stream
-                        // and because we want the browser to consume the response
-                        // as well as the cache consuming the response, we need
-                        // to clone it so we have two streams.
-                        var responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
+                return response || fetch(event.request)
+                    .then(function (response) {
+                        return caches.open(urlsToCache)
                             .then(function (cache) {
-                                cache.put(event.request, responseToCache);
-                            });
+                                cache.put(event.request, response.clone());
+                                return response;
+                            })
+                            .catch(function () {
 
-                        return response;
-                    }
-                );
+                                return caches.match('/index.html');
+                            });
+                    });
             })
     );
 });
-self.addEventListener('activate', function(event) {
-
-    var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-  
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cacheName) {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
     );
-  });
-  
+    return self.clients.claim();
+});
